6fb19f1b8b46b6cd3693cff4693ec715eb27112c
bolyb <brianboly@gmail.com>
2015-11-17 16:37:54 -0800

    Create ZipCode.py
    v1

---
+++ ZipCode.py
@@ -0,0 +1,19 @@
+#author = Max Dimm
+#date = 11/17/2015
+#email = dimmm@oregonstate.edu
+
+s = "You can find help therapy at the Corvallis Clinic at the end of 9th Street heading North"
+t = "Sorry future development is nessesary and we do not have data for your location"
+
+corvallis = 97330
+
+print "Hello user, This program is built to help you find a location to recieve therapy based off your location."
+
+area = int(input("Enter your area code: "))
+if area == corvallis:
+	print s
+	
+else:
+	print t
+	
+print "Goodbye"
